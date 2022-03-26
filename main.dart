271dff8671465c6c334b28193afc2a7a2d4b5c5e
angelgm20 <97719824+angelgm20@users.noreply.github.com>
2022-03-25 21:06:13 -0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:appmonkey/src/routes/routes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Monkey Delivery',
+    initialRoute: '/',
+    routes: routes,
+    theme: ThemeData(
+        primaryColor: const Color.fromRGBO(252, 96, 17, 1.0),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: const Color.fromRGBO(74, 75, 77, 1.0),
+        ),
+        fontFamily: 'Cobin'
+        //accentColor: const Color.fromRGBO(74, 75, 77, 1.0)
+        ),
+  ));
+}
